37f2f543bf4dc5ac08b492259eb072301d73e910
patrick snowden <patrickbsnowden@outlook.com>
2022-05-16 17:42:53 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import qrCode from "./images/image-qr-code.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper w-96 mx-auto p-5 rounded-xl">
+        <img src= {qrCode} alt="" className="rounded-xl mb-5" />
+        <div className="textContent p-2 text-center">
+          <h1 className="font-bold mb-5 leading-tight">Improve your front-end skills by building projects</h1>
+          <p className="mb-5 leading-tight">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </>
   );
 }
 
